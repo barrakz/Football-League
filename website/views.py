d83c2afb21bd571ce212244c9968efe44f8ed2da
barrakz <b.rakuzy@gmail.com>
2023-03-01 20:38:30 +0100

    add player and rate player views and templates added

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, render_template, redirect, url_for
 from flask_login import current_user, login_required
 
-from .models import Team
+from .models import Team, Player, Rating
 from . import db
 
 views = Blueprint("views", __name__)
@@ -23,3 +23,33 @@ def add_team():
         db.session.commit()
         return redirect(url_for("views.home"))
     return render_template("add_team.html", user=current_user)
+
+
+@views.route("/addplayer", methods=["GET", "POST"])
+@login_required
+def add_player():
+    if request.method == 'POST':
+        name = request.form["name"]
+        team_id = request.form["team"]
+        new_player = Player(name=name, team_id=team_id)
+        db.session.add(new_player)
+        db.session.commit()
+        return redirect(url_for("views.home"))
+
+    teams = Team.query.all()
+    return render_template("add_player.html", teams=teams, user=current_user)
+
+
+@views.route("/rateplayer/<int:player_id>", methods=["GET", "POST"])
+@login_required
+def rate_player(player_id):
+    player = Player.query.get_or_404(player_id)
+    if request.method == 'POST':
+        rating = request.form["rating"]
+        comment = request.form["comment"]
+        new_rating = Rating(rating=rating, comment=comment, player_id=player.id, user_id=current_user.id)
+        db.session.add(new_rating)
+        db.session.commit()
+        return redirect(url_for("views.home", player_id=player_id))
+
+    return render_template("rate_player.html", player=player, user=current_user)
